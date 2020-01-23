832b925afa1134cab0fa62555e654ec460eb6b2f
Michael Donahue <michaeldonahue17@gmail.com>
2020-01-23 13:17:56 -0500

    Added Routing and Main Page Elements

--- full-stack-website/src/App.js
+++ full-stack-website/src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import FeaturedPost from './components/FeaturedPost';
+import MainPost from './components/MainPost';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container">
+      <div style={{padding: "10px"}}>
+        <MainPost />
+      </div>
+      <div class="row">
+        <div style={{padding: "10px"}} class="col-7">
+          <FeaturedPost />
+        </div>
+        <div style={{padding: "10px"}} class="col-5">
+          <FeaturedPost />
+        </div>
+      </div>
+
     </div>
   );
 }
---
+++ full-stack-website/src/components/FeaturedPost.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './FeaturedPost.css'
+
+class FeaturedPost extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div id="featuredDiv" class="container-fluid">
+                Hello
+            </div>
+         );
+    }
+}
+ 
+export default FeaturedPost;
